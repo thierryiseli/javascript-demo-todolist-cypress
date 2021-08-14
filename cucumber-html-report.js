f36625fe8b3eb9a3ee2081e2fb4b66461b4e5544
thierryiseli <me@thierryiseli.com>
2021-08-15 00:36:18 +0200

    [FIX] Try to optimize html report

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -1,12 +1,47 @@
 const report = require('multiple-cucumber-html-reporter');
+const fs = require('fs');
 
-report.generate({
-	jsonDir: './cypress/result/',
-	reportPath: './cypress/report/',
-    customData: {
-        title: 'Run info',
-        data: [
-            {label: 'Project', value: 'Todolist'}
-        ]
+const mapOs = (os) => {
+    if(os.startsWith('win')) {
+        return 'windows';
+    } else if (os.startsWith('osx')) {
+        return 'osx';
+    } else if (os.startsWith('linux')) {
+        return 'linux';
+    } else if (os.startsWith('ubuntu')) {
+        return 'ubuntu';
+    } else if (os.startsWith('android')) {
+        return 'android';
+    } else if (os.startsWith('ios')) {
+        return 'ios';
     }
+};
+
+fs.readFile('cypress/.run/results.json', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    var runInfos = JSON.parse(data);
+    report.generate({
+        jsonDir: './cypress/result/',
+        reportPath: './cypress/report/',
+        metadata:{
+            browser: {
+                name: runInfos.browserName,
+                version: runInfos.browserVersion
+            },
+            device: 'Cypress',
+            platform: {
+                name: mapOs(runInfos.osName)
+            }
+        },
+        customData: {
+            title: 'Run info',
+            data: [
+                {label: 'Project', value: 'Todolist'},
+                {label: 'Execution Start Time', value: new Date(runInfos.startedTestsAt).toLocaleString()},
+                {label: 'Execution End Time', value: new Date(runInfos.endedTestsAt).toLocaleString()}
+            ]
+        }
+    });
 });
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,12 @@
 const cucumber = require('cypress-cucumber-preprocessor').default
+const fs = require('fs');
 
 module.exports = (on, config) => {
-  on('file:preprocessor', cucumber())
+  on('file:preprocessor', cucumber()),
+  on('after:run', (results) => {
+    if (results) {
+      fs.mkdirSync("cypress/.run", { recursive: true });
+      fs.writeFile("cypress/.run/results.json", JSON.stringify(results));
+    }
+  })
 }
